75e7eb7e345877d9b64ab3c953882ce6eb6314ac
Manogel <manoelgomes53@gmail.com>
2021-06-02 09:07:48 -0300

    refactor: add comments

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig = getAsyncAppConfig();
-  const PORT = '4000';
+  // Deixei uma variavel ambiente como melhoria
+  const PORT = appConfig.port;
 
   await app.listen(PORT, () => {
     console.log(`🚀 App is running on port ${PORT}`);

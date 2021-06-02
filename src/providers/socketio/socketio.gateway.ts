37d1204588cb8ba6c6632917aed3d43b7c8abb6b
henrickJona <jhenrick41@gmail.com>
2021-06-02 11:52:55 -0300

    feat : add broadcast method

--- src/providers/socketio/socketio.gateway.ts
+++ src/providers/socketio/socketio.gateway.ts
@@ -1,4 +1,16 @@
-import { WebSocketGateway } from '@nestjs/websockets';
-
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 @WebSocketGateway()
-export class SocketGateway {}
+export class SocketGateway {
+  @WebSocketServer() server: Server;
+  emit(event: string, data: any, channel?: string) {
+    if (channel) {
+      this.server.to(channel).emit(event, data);
+    } else {
+      this.server.emit(event, data);
+    }
+  }
+  broadcast(event: string, data: any) {
+    this.emit(event, data);
+  }
+}

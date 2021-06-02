fbe978d455055fa8167927fbaaacd78d85c95a01
henrickJona <jhenrick41@gmail.com>
2021-06-02 16:53:29 -0300

    feat: add message module

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { getAsyncAppConfig } from '@config/app';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig = getAsyncAppConfig();
   // Deixei uma variavel ambiente como melhoria
   const PORT = appConfig.port;
-
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(PORT, () => {
     console.log(`🚀 App is running on port ${PORT}`);
   });
---
+++ src/modules/message/dto/send-message-text.dto.ts
@@ -0,0 +1,4 @@
+export class SendMessageTextDto {
+  to: string;
+  message: string;
+}
